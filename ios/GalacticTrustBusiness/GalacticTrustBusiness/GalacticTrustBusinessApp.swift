b10ae09bc17b840eb443877e4cb29a12da65ac54
crypto-mint <hartensteindominic@gmail.com>
2026-08-31 21:43:57 -0400

    Preserve existing financial data

--- ios/GalacticTrustBusiness/GalacticTrustBusiness/GalacticTrustBusinessApp.swift
+++ ios/GalacticTrustBusiness/GalacticTrustBusiness/GalacticTrustBusinessApp.swift
@@ -3,18 +3,12 @@ import SwiftUI
 @main
 struct GalacticTrustBusinessApp: App {
     @StateObject private var store = FinancialStore()
-    @AppStorage("didInitializeProductionWorkspace") private var didInitializeProductionWorkspace = false
 
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(store)
                 .preferredColorScheme(.light)
-                .task {
-                    guard !didInitializeProductionWorkspace else { return }
-                    store.clearFinancialData()
-                    didInitializeProductionWorkspace = true
-                }
         }
     }
 }
